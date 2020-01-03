42e81c6e78b414b148df00a92eb88b7a54e5d1a5
K-weiya <1191262176@qq.com>
2020-01-03 18:40:27 +0800

    first commit

---
+++ jump7.py
@@ -0,0 +1,16 @@
+for a in range(1,101):
+        if a % 10 == 7 or a // 10 == 7 or a % 7 == 0:
+                    continue
+                    print(a)
+
+#a=0
+#while a < 100:
+#    a = a+1
+#    if a % 10 == 7 or a // 10 == 7 or a % 7 == 0:
+#        continue
+#    elif a // 10 == 7 or a % 7 == 0:
+#        continue
+#    elif a % 7 == 0:
+#        continue
+#    else
+#    print(a)
